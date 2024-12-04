3a2d6cc0962cbd939f837c13cf44d5442e5a6356
Kobby5555 <kobby55885000@gmail.com>
2024-12-04 13:40:36 +0000

    Update code.js
    I used a switch. I made sure the box doesn't move out of the border

--- code.js
+++ code.js
@@ -16,19 +16,21 @@ else if(event.key === "ArrowUp"){
         x_axis -= speed;}
         else if(event.key === "ArrowRight"){
             x_axis += speed;}
-        if(y_axis<0) {
-            y_axis = 0;
-        }
-        if(y_axis>parentHeight-ballHeight) {
-            y_axis = parentHeight-ballHeight;
-        }
-        if(x_axis<0) {
-            x_axis = 0;
-        }
-        if(x_axis>parentWidth-ballHeight) {
-            x_axis = parentWidth-ballHeight;
-        }
-        child[0].style.top= y_axis+"px";
-        child[0].style.left = x_axis+"px";
 
-})
+            switch (true){
+                case y_axis< 0:
+                    y_axis = 0;
+                    break;
+                    case y_axis>parentHeight-ballHeight:
+                        y_axis = parentHeight-ballHeight;
+                        break;
+                        case x_axis<0:
+                            x_axis = 0;
+                            break;
+                            case x_axis>parentWidth-ballHeight:
+                                x_axis = parentWidth-ballHeight;
+                                break;
+                                default: 
+                                child[0].style.top = `${y_axis}px`;
+                                child[0].style.left = `${x_axis}px`;
+            }})
